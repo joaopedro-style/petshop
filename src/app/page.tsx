e45cde600c0f15a1297ede6a551ab1ebedd7cde1
joaopedro-style <joaopedro18231@hotmail.com>
2025-08-01 09:42:01 -0300

    Adiciona ListaPosts e estilos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // src/app/page.tsx
+import ListaPosts from "@/components/ListaPosts";
 import estilos from "./page.module.css";
 export default function Home() {
   return (
     <section className={estilos.conteudo}>
       <h2>Pet Notícias</h2>
-      <p>Saiba mais sobres os pets</p>
+      <p>Aqui você encontra as últimas notícias sobre Pets.</p>
+      <ListaPosts />
     </section>
   );
 }
