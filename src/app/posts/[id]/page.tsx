6d358204a2b3206ab0ecda7f0b866b617c966e25
joaopedro-style <joaopedro18231@hotmail.com>
2025-08-04 11:22:05 -0300

    Carrega os dados do post e desestrutura

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,18 +1,32 @@
 // src/app/posts/[id]/page.tsx
 import estilos from "./detalhe-post.module.css";
 import Container from "@/components/Container";
+import { Post } from "@/types/Post";
 
 type DetalhePostProps = {
   params: Promise<{ id: string }>;
 };
 
-export default function DetalhePost({ params }: DetalhePostProps) {
+export default async function DetalhePost({ params }: DetalhePostProps) {
+  const { id } = await params;
+
+  const resposta = await fetch(`http://localhost:2112/posts/${id}`, {
+    next: { revalidate: 0 },
+  });
+
+  if (!resposta.ok) {
+    throw new Error("Erro ao buscar o post: " + resposta.statusText);
+  }
+
+  const post: Post = await resposta.json();
+  const { titulo, categoria, descricao } = post;
+
   return (
     <article className={estilos.conteudo}>
-      <h2>Título...</h2>
+      <h2>{titulo}</h2>
       <Container>
-        <h3>Categoria...</h3>
-        <p>Descrição...</p>
+        <h3>{categoria}</h3>
+        <p>{descricao}</p>
       </Container>
     </article>
   );
