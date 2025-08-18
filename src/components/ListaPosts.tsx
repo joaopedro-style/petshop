a1e5a99048bcfe0a56319336ac731175909a7d55
joaopedro-style <joaopedro18231@hotmail.com>
2025-08-18 09:08:45 -0300

    defini tipos

--- src/components/ListaPosts.tsx
+++ src/components/ListaPosts.tsx
@@ -16,7 +16,8 @@ export default function ListaPosts({ posts }: ListaPostsProps) {
   // Gerando um novo array de categorias usando map e garantindo que não há repetição de categorias usando spread e new Set.
   const categorias = [...new Set(posts.map((post) => post.categoria))];
 
-  const [categoriaAtiva, setCategoriaAtiva] = useState();
+  /* Definindo o state com tipos null (quando não há categoria selecionada) ou string (que é tipo para nomes/textos referentes às categorias). Passamos null entre parênteses indicando que por padrão não há categoria selecionada. */
+  const [categoriaAtiva, setCategoriaAtiva] = useState<null | string>(null);
 
   return (
     <>
